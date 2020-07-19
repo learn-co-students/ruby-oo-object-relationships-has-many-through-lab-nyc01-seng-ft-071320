00f7b9f2497c97bde0c1a95238febfb79a81c757
woink <wardprice@gmail.com>
2020-07-19 11:14:46 -0400

    Done.

--- lib/console.rb
+++ lib/console.rb
@@ -4,9 +4,5 @@ require_relative './patient.rb'
 require 'pry'
 
 
-doogie = Doctor.new("Doogie Howser")
-ferris = Patient.new("Ferris Bueller")
-new_appointment = Appointment.new('January 1st, 1989', ferris, doogie)
 
-
-binding.pry
+# binding.pry
